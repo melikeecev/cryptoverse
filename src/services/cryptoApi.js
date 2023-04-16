7a3f0ca2cbb8cbdffbed077f552e1cc5e20d7ebb
Melike Çevik <melike_cevik1@outlook.com>
2023-04-16 11:59:26 +0300

    cryptocurrencies page done

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,24 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+const cryptoApiHeaders = {
+    'X-RapidAPI-Key': 'c4ff3cdca5msh9652e4554d98f67p1a9391jsnd173b152850e',
+    'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+
+const createRequest = (url) => ({url, headers: cryptoApiHeaders})
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: (count) => createRequest(`/coins?limit=${count}`),
+        })
+    })
+});
+
+export const {
+    useGetCryptosQuery,
+} = cryptoApi;
